625e6c9d10152d26b131c7e106885aa4d39d78dd
gguio <gdguio@outlook.com>
2024-02-28 00:51:42 -0500

    Succesful visits after QR scan

--- web/src/pages/scanner.js
+++ web/src/pages/scanner.js
@@ -11,7 +11,6 @@ class Scanner extends BindingClass {
     super();
     this.bindClassMethods(["mount", "loadScanner"], this);
     this.dataStore = new DataStore();
-    // this.dataStore.addChangeListener(this.redirectToViewPlaylist);
     this.header = new Header(this.dataStore);
   }
 
@@ -19,64 +18,21 @@ class Scanner extends BindingClass {
    * Add the header to the page and load the Client.
    */
   async mount() {
-    // document.getElementById("startScanning").addEventListener("click", this.loadScanner);
-
+    document.getElementById("startScanning").addEventListener("click", this.loadScanner);
     this.header.addHeaderToPage();
     this.client = new RaffleClient();
-    const visit = await this.client.createVisit("Starbucks", "gabeguio@gmail.com", "Gabe Guio", "Company", (error) => {
-      console.log(error.message);
-    });
-    console.log(visit);
     // this.loadScanner();
   }
 
-  /**
-   * Method to run when the create playlist submit button is pressed. Call the MusicPlaylistService to create the
-   * playlist.
-   */
-  // async submit(evt) {
-  //   evt.preventDefault();
-  //   const errorMessageDisplay = document.getElementById("error-message");
-  //   errorMessageDisplay.innerText = ``;
-  //   errorMessageDisplay.classList.add("hidden");
-  //   const createButton = document.getElementById("create");
-  //   const origButtonText = createButton.innerText;
-  //   createButton.innerText = "Loading...";
-  //   const playlistName = document.getElementById("playlist-name").value;
-  //   const tagsText = document.getElementById("tags").value;
-  //   let tags;
-  //   if (tagsText.length < 1) {
-  //     tags = null;
-  //   } else {
-  //     tags = tagsText.split(/\s*,\s*/);
-  //   }
-  //   const playlist = await this.client.createPlaylist(playlistName, tags, (error) => {
-  //     createButton.innerText = origButtonText;
-  //     errorMessageDisplay.innerText = `Error: ${error.message}`;
-  //     errorMessageDisplay.classList.remove("hidden");
-  //   });
-  //   this.dataStore.set("playlist", playlist);
-  // }
-
-  /**
-   * When the playlist is updated in the datastore, redirect to the view playlist page.
-   */
-  // redirectToViewPlaylist() {
-  //   const playlist = this.dataStore.get("playlist");
-  //   if (playlist != null) {
-  //     window.location.href = `/playlist.html?id=${playlist.id}`;
-  //   }
-  // }
-
-  loadScanner(scannerEmail) {
+  loadScanner() {
     // create a div with id reader and add to the scanner section
-    // const reader = document.createElement("div");
-    // reader.id = "reader";
-    // document.getElementById("scanner").appendChild(reader);
-    // const writer = document.getElementById("writer");
-    // writer.innerHTML = "";
+    const reader = document.createElement("div");
+    reader.id = "reader";
+    document.getElementById("scanner").appendChild(reader);
+    const writer = document.getElementById("writer");
+    writer.innerHTML = "";
 
-    function parseVCard(decodedText) {
+    const parseVCard = (decodedText) => {
       const lines = decodedText.split("\n");
       const vCard = {};
 
@@ -90,41 +46,36 @@ class Scanner extends BindingClass {
       });
 
       return vCard;
-    }
+    };
 
-    async function onScanSuccess(decodedText, decodedResult) {
+    const onScanSuccess = async (decodedText, decodedResult) => {
       const vCardObject = parseVCard(decodedText);
-      console.log(vCardObject);
+
+      const { email, fn = "N/A", org = "N/A" } = vCardObject;
+
       const sponsorName = "DataTuneConf";
 
-      console.log(sponsorName, vCardObject.email, vCardObject.fn, vCardObject.org);
+      try {
+        //start spinner
+        const spinner = document.querySelector(".loader");
+        spinner.style.display = "block";
+        const visit = await this.client.createVisit(sponsorName, email, fn, org);
+        //stop spinner
+        spinner.style.display = "none";
+        document.getElementById("writer").innerHTML = `<p>${visit.visitorFullName} has been checked in!</p>`;
+      } catch (error) {
+        console.error(error.message);
+        // Handle error appropriately
+      }
 
-      // const errorMessageDisplay = document.getElementById("error-message");
-      // errorMessageDisplay.innerText = ``;
-      // errorMessageDisplay.classList.add("hidden");
-      const visit = await this.client.createVisit(sponsorName, vCardObject.email, vCardObject.fn, vCardObject.org, (error) => {
-        console.log(error.message);
-        // createButton.innerText = origButtonText;
-        // errorMessageDisplay.innerText = `Error: ${error.message}`;
-        // errorMessageDisplay.classList.remove("hidden");
-      });
-
-      console.log(visit);
-
-      document.getElementById("writer").innerHTML = `<p>${visit.visitorFullName} from ${visit.visitorOrganization} has been checked in!</p>`;
-      // ...
       // this will stop the scanner (video feed) and clear the scan area.
       html5QrcodeScanner.clear();
-    }
+    };
 
     let html5QrcodeScanner = new Html5QrcodeScanner("reader", { fps: 10, qrbox: { width: 200, height: 200 } }, /* verbose= */ false);
 
     html5QrcodeScanner.render(onScanSuccess);
   }
-}
-
-async function createVisit(vCardObject, sponsorName) {
-  return visit;
 }
 
 /**
